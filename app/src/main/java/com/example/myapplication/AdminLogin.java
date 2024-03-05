176e2dcb7009f9f3979a8ff7f6922aa8ab19b773
Dinesh Racharla <96189870+DineshRacharla468@users.noreply.github.com>
2024-03-04 21:05:31 -0600

    Created Admin login page, page allows admin to login with user name and password. after login admin interface will appear

--- app/src/main/java/com/example/myapplication/AdminLogin.java
+++ app/src/main/java/com/example/myapplication/AdminLogin.java
@@ -14,5 +14,33 @@ public class AdminLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+
+        Button adminloginBTN = findViewById(R.id.adminloginBTN);
+        adminloginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AdminInterface.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView admintoSignUp = findViewById(R.id.admintoSignUp);
+        admintoSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterPage.class);
+                startActivity(intent);
+            }
+        });
+
+
+        TextView aUserlogin = findViewById(R.id.aUserlogin);
+        aUserlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
