987e502721b5f933d016ad82eafe08b31dc4b335
BhanuGorantla <143116800+BhanuGorantla@users.noreply.github.com>
2024-03-28 21:47:29 -0500

    s565092_bhanu_venkata_siva_kumar_gorantla
    When user Click on the logout, user will be logged out of the application, we used google firebase authentication for logout. then it will redirect to the admin login page.

--- app/src/main/java/com/example/myapplication/AdminInterface.java
+++ app/src/main/java/com/example/myapplication/AdminInterface.java
@@ -7,12 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AdminInterface extends AppCompatActivity {
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_interface);
+
+        mAuth = FirebaseAuth.getInstance();
+        Button adminSignout = findViewById(R.id.adminlogoutBTN);
+        adminSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent ini = new Intent(getApplicationContext(), AdminLogin.class);
+                startActivity(ini);
+                finish();
+            }
+        });
+
+
+
+
+
 
         Button adminsearchBTN = findViewById(R.id.adminsearchBTN);
         adminsearchBTN.setOnClickListener(new View.OnClickListener() {
