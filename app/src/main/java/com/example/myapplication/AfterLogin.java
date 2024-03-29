e16c967cf5629c0c369101bf7768ed061d6a9975
kavyasree05 <137372024+kavyasree05@users.noreply.github.com>
2024-03-28 21:37:05 -0500

    Kavya Sree Konda    S558899  Team - 04    03-28-2024
    When click on the logout the user will be logout from the app and navigated to the login page.

--- app/src/main/java/com/example/myapplication/AfterLogin.java
+++ app/src/main/java/com/example/myapplication/AfterLogin.java
@@ -7,12 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AfterLogin extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_login);
+
+        mAuth =FirebaseAuth.getInstance();
+        Button logoutBTN = findViewById(R.id.logoutBTN);
+        logoutBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent ini = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(ini);
+                finish();
+            }
+        });
 
         Button searchBTN = findViewById(R.id.searchBTN);
         searchBTN.setOnClickListener(new View.OnClickListener() {
