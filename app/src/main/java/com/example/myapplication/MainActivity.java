61a7499754f12e4be329bf54eb67c5e6c9879a98
Sri Sai Krishna Bathula <S562321@nwmissouri.edu>
2024-03-28 21:06:38 -0500

    S562321 - Sri Sai Krishna Bathula
    Added authentication to the login page using Google Firebase Authentication, User can login and registering into the app

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,28 +1,49 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText emailET;
+    EditText passwordET;
+    Button loginBTN;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginBTN = findViewById(R.id.loginBTN);
+        emailET = findViewById(R.id.UserNameET);
+        passwordET = findViewById(R.id.passwordPW);
+        loginBTN = findViewById(R.id.loginBTN);
+
+        mAuth = FirebaseAuth.getInstance();
+
         loginBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), AfterLogin.class);
-                startActivity(intent);
+                String email = emailET.getText().toString().trim();
+                String password = passwordET.getText().toString().trim();
+
+                signIn(email, password);
             }
         });
+
 
         TextView toSignUp = findViewById(R.id.toSignUp);
         toSignUp.setOnClickListener(new View.OnClickListener() {
@@ -42,5 +63,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    // Here you can proceed to the AfterLogin activity
+                    Intent intent = new Intent(MainActivity.this, AfterLogin.class);
+                    startActivity(intent);
+                    finish(); // Finish the current activity to prevent the user from going back to the login screen
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(MainActivity.this, "Authentication", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+
+    }
+
+
 }
